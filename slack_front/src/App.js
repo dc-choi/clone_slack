d7c682a91c3b40bdd33a42053903b7c516eaf156
Hayeong8957 <judy.hayoung.shin@gmail.com>
2021-12-15 14:46:00 +0900

    temp

--- slack_front/src/App.js
+++ slack_front/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          이메일로 로그인해 보세요
+        </h1>
+        <p>직장에서 사용하는 이메일 주소로 로그인하는 걸 추천드려요.</p>
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
