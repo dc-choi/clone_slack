efa46b90bafa2ef1c0be203ab074b20c191fe2e6
Hayeong8957 <judy.hayoung.shin@gmail.com>
2022-01-18 21:37:25 +0900

    Modified LoginGoogle.js, Send Tokens

--- src/components/LoginGoogle.js
+++ src/components/LoginGoogle.js
@@ -2,17 +2,28 @@ import React, { useState } from "react";
 import "../css/LoginGoogle.css";
 import { useNavigate } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
+import axios from "axios";
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 function LoginGoogle() {
-  const [showloginButton, setShowloginButton] = useState(true);
   const navigate = useNavigate();
 
+  // 서버 요청
   const onLoginSuccess = (res) => {
-    console.log("Login Success:", res.profileObj);
-    setShowloginButton(false);
-    navigate("/SetupWorkspace");
+    axios({
+      method: "post",
+      url: "http://localhost:9000/api/auth/googleLogin",
+      data: {
+        profileObj: "res.profileObj",
+        tokenObj: "res.tokenObj",
+      },
+    })
+      .then((res) => {
+        navigate("./SetupWorkspace");
+        console.log("Login Success: ", res.profileObj);
+      })
+      .catch((error) => console.log("error: ", error.res));
   };
 
   const onLoginFailure = (res) => {
@@ -21,18 +32,16 @@ function LoginGoogle() {
 
   return (
     <>
-      {showloginButton ? (
-        <GoogleLogin
-          className="c-button c-button--primary c-button--large c-third_party_auth c-google_login full_width"
-          type="button"
-          clientId={clientId}
-          buttonText="Google 계정으로 로그인"
-          onSuccess={onLoginSuccess}
-          onFailure={onLoginFailure}
-          cookiePolicy={"single_host_origin"}
-          isSignedIn={true}
-        />
-      ) : null}
+      <GoogleLogin
+        className="c-button c-button--primary c-button--large c-third_party_auth c-google_login full_width"
+        type="button"
+        clientId={clientId}
+        buttonText="Google 계정으로 로그인"
+        onSuccess={onLoginSuccess}
+        onFailure={onLoginFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
     </>
   );
 }
