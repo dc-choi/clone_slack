b4a6e9dfd08dfd5369a059f3ab24edbdb98cdb5a
su1715 <su1715@sookmyung.ac.kr>
2022-02-09 16:03:52 +0900

    update: Workspaces.js 더미데이터 constants폴더로 옮김

---
+++ src/constants/userWorkspacesData.js
@@ -0,0 +1,26 @@
+//Workspaces 더미데이터
+export const workspace_objs = [
+  {
+    id: 0,
+    title: "[5.2]42s_Piscine",
+    pesonnel: 310,
+    to: "/",
+  },
+  {
+    id: 1,
+    title: "Slack",
+    pesonnel: 1,
+    to: "/",
+  },
+  {
+    id: 2,
+    title: "Slack",
+    pesonnel: 1,
+    to: "/",
+  },
+];
+export const invited_objs = [
+  { id: 0, title: "K-BioX", personnel: 2777, to: "/" },
+  { id: 1, title: "Uni-D", personnel: 228, to: "/" },
+];
+export const email = "su1715@sookmyung.ac.kr";
