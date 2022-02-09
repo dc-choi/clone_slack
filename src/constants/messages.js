30efd5cd7ed91222e3de533228c530dba61e8d5d
su1715 <su1715@sookmyung.ac.kr>
2022-02-09 13:52:23 +0900

    refactoring: 경고메세지, 이메일 검사 정규표현식을 constants 폴더로 이동

---
+++ src/constants/messages.js
@@ -0,0 +1,4 @@
+export const SIGNUP_ALERT_EMPTY = "이는 필수이며 이메일을 입력해야 합니다.";
+export const SIGNUP_ALERT_INVALID = "이메일 주소가 유효하지 않은 것 같습니다.";
+export const SIGNIN_ALERT_EMPTY = "이메일을 입력해주세요.";
+export const SIGNIN_ALERT_INVALID = "죄송합니다. 이메일이 유효하지 않습니다.";
---
+++ src/constants/regEmail.js
@@ -0,0 +1,2 @@
+export const rEmail =
+  /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
