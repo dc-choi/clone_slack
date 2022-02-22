c70538a57ee4e55c096c0f0d9155642263bbcb2d
Hayeong8957 <judy.hayoung.shin@gmail.com>
2022-02-22 16:23:51 +0900

    Create Header.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignUp from "./SignUp";
 import SetupWorkspace from "./SetupWorkspace";
 import SetupChannel from "./SetupChannel";
 import Workspaces from "./Workspaces";
-import Main from "./Main";
+import WorkspaceMain from "./WorkspaceMain";
 import "./css/App.css";
 
 function App() {
@@ -18,7 +18,8 @@ function App() {
         <Route path="/Workspaces" element={<Workspaces />} />
         <Route path="/SetupWorkspace" element={<SetupWorkspace />} />
         <Route path="/SetupChannel" element={<SetupChannel />} />
-        <Route path="/Main/:workspaceId" element={<Main />} />
+        <Route path="/WorkspaceMain" element={<WorkspaceMain />} />
+        <Route path="/WorkspaceMain/:workspaceId" element={<WorkspaceMain />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/WorkspaceMain.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import "./css/WorkspaceMain.css";
+
+function WorkspaceMain() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default WorkspaceMain;
